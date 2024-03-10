59c98f67ce8fb136e61e8e98399648de9cb837e2
mostafa mohmad <111007392+mostafamohmad399@users.noreply.github.com>
2024-03-10 12:18:53 +0300

    Update degrees.py

--- projects/2024/x/degrees.py
+++ projects/2024/x/degrees.py
@@ -1,1 +1,11 @@
+import datetime
+minute_late = None
+while True:
+    current_time = datetime.datetime.now()
+    minute = current_time.minute
 
+    if minute != minute_late:
+        print(minute)
+        minute_late = minute
+
+#
